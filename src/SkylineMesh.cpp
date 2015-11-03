749692695d215d2d96a79e3cee9a04357ebc1bb1
dazotaro <dazotaro@gmail.com>
2015-11-03 23:07:25 +0100

    Added Terrain/Skyline Mesh

---
+++ src/SkylineMesh.cpp
@@ -0,0 +1,47 @@
+/*
+ * SkylineMesh.cpp
+ *
+ *  Created on: Oct 29, 2015
+ *      Author: jusabiaga
+ */
+
+// Local includes
+#include "SkylineMesh.hpp"
+#include "../graphics/gl_core_4_2.hpp"		// openGL
+// Global includes
+#include "cmath"							// std::cos
+
+namespace JU
+{
+
+SkylineMesh::SkylineMesh()
+{
+	const uint32 num_vertices = 5;
+	glm::vec2 vertexPositions[num_vertices];
+
+	vertexPositions[0][0] = -5.0f;
+	vertexPositions[0][1] =  0.0f;
+	vertexPositions[1][0] = -2.5f;
+	vertexPositions[1][1] = -2.0f;
+	vertexPositions[2][0] =  0.0f;
+	vertexPositions[2][1] =  3.0f;
+	vertexPositions[3][0] =  2.5f;
+	vertexPositions[3][1] = -1.0f;
+	vertexPositions[4][0] =  5.0f;
+	vertexPositions[4][1] =  0.0f;
+
+	uint32 vertexIndices[num_vertices];
+
+	for (uint32 i = 0; i < num_vertices; ++i)
+	{
+		vertexIndices[i] = i;
+	}
+
+    setData(vertexPositions, num_vertices, vertexIndices, num_vertices, gl::LINE_STRIP);
+}
+
+SkylineMesh::~SkylineMesh()
+{
+}
+
+} /* namespace JU */
---
+++ src/SkylineMesh.hpp
@@ -0,0 +1,25 @@
+/*
+ * SkylineMesh.hpp
+ *
+ *  Created on: Oct 29, 2015
+ *      Author: jusabiaga
+ */
+
+#ifndef SKYLINEMESH_HPP_
+#define SKYLINEMESH_HPP_
+
+#include "../graphics/Mesh2D.hpp"	// Mesh2D
+
+namespace JU
+{
+
+class SkylineMesh : public Mesh2D
+{
+	public:
+		SkylineMesh();
+		virtual ~SkylineMesh();
+};
+
+} /* namespace JU */
+
+#endif /* SKYLINEMESH_HPP_ */
