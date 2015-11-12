d80125b7314c5f77e3ce772e3f6a4c5756c48604
dazotaro <dazotaro@gmail.com>
2015-11-12 21:42:57 +0100

    Fixed indentation

--- data/shaders/simple.fs
+++ data/shaders/simple.fs
@@ -5,6 +5,6 @@ out vec4 outputColor;
 
 void main()
 {
-   outputColor = vcolor;
-   //outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);;
+    outputColor = vcolor;
+    //outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);;
 }
